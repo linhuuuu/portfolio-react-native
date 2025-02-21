349c47cfcb25710c4e9f481ad43f898f4485ff24
Arturo <arturo_andres_desilva@dlsl.edu.ph>
2025-02-21 12:35:51 +0800

    react native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, FlatList, Button, Image, SectionList, Switch } from 'react-native';
+import React, {useState} from 'react';
 export default function App() {
+
+  const [theme, SetTheme] = useState(true);  //true = lightmode; false = darkmode
+  const toggleSwitch = ()=> {SetTheme(!theme);}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+    <View style={[styles.container, theme === false ? styles.darkmode : styles.lightmode]}>
       <StatusBar style="auto" />
+      <View>
+        <Image source={require('./assets/favicon.png')} />
+        <Text style={theme === false ? styles.darkmode : styles.lightmode}>Arturo Andres</Text>
+      </View>
+
+      <View>
+        <Text style={[theme === false ? styles.darkmode : styles.lightmode]}>Description</Text>
+      </View>
+
+      <View>
+        <SectionList
+        
+        sections={[
+          {title: 'Skills', data: ['Art', 'Programming', 'Writing'] },
+          {title: 'Contact Info', data: ['Email'] },
+        ]}
+          
+          renderItem={({ item }) => <Text style={[theme === false ? styles.darkmode : styles.lightmode]}>{item}</Text>}
+          renderSectionHeader ={({ section }) => <Text style={[theme === false ? styles.darkmode : styles.lightmode]}> {section.title}</Text>}
+        />
+      </View>
+
+      <View>
+        <FlatList
+        style={[theme === false ? styles.darkmode : styles.lightmode]}
+        data={[{key: 'One'}, {key: 'Two'}]}
+        renderItem={({item}) => <Button title={item.key} ></Button>}
+        />
+      </View>
+      
+      <View>
+        <Switch
+        trackColor={{false: '#767577' , true: '#81b0ff'}}
+        thumbColor={'#f5dd4b'}
+        ios_backgroundColor={'#f5dd4b'}
+        onValueChange={toggleSwitch}
+        value={theme}
+        
+        />
+      </View>
+{/**
+      <View>
+        <FlatList
+          data={[{ key: 'Alice' }, { key: 'Bob' }]}
+          renderItem={({ item }) => <Button title={item.key}></Button>}
+        />
+      </View>
+
+ */}
+      
     </View>
+
   );
 }
 
@@ -17,4 +71,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  darkmode: {
+    color: '#fff',
+    backgroundColor: '#000'
+  },
+
+  lightmode: {
+    color: '#000',
+    backgroundColor: '#fff'
+  }
+
 });
